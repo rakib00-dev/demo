b6a0695b9841906a9f3288c5ac059ea8e36f1c59
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-10-21 21:50:35 +0600

    fix the navscroll effect and contact form and wrapper and more

--- src/main.js
+++ src/main.js
@@ -1,23 +1,27 @@
+const fixedNav = document.getElementById('fixedNav');
 const bar = document.getElementById('bar');
 const cross = document.getElementById('cross');
 const menuUl = document.getElementById('menuUl');
 
+window.onload = () => {
+  const width = window.innerWidth;
+  fixedNav.style.width = `${width}px`;
+};
+
 window.addEventListener('resize', () => {
   const width = window.innerWidth;
-  console.log(width);
 
   if (width <= 1024) {
     menuUl.style.display = 'none';
-    console.log('ok');
+    fixedNav.style.width = `${width}px`;
     menuUl.classList.add('navToggle');
   } else {
     menuUl.classList.remove('navToggle');
     menuUl.style.display = 'flex';
+    fixedNav.style.width = '';
   }
 });
 
-// console.log(width);
-console.log(menuUl);
 bar.addEventListener('click', () => {
   cross.style.display = 'block';
   bar.style.display = 'none';
@@ -26,7 +30,26 @@ bar.addEventListener('click', () => {
   cross.addEventListener('click', () => {
     cross.style.display = 'none';
     bar.style.display = 'block';
-    menuUl.style.display = 'none';
     menuUl.classList.remove('navToggle');
+    menuUl.style.display = 'none';
   });
+});
+
+// navBar onScroll
+
+window.addEventListener('scroll', function () {
+  const scrollCount = window.scrollY;
+  if (scrollCount >= 82) {
+    fixedNav.style.top = '-15px';
+    fixedNav.style.boxShadow = '0px -14px 25px 2px #11111199';
+    fixedNav.style.position = 'sticky';
+  } else if (scrollCount >= 92) {
+    fixedNav.style.top = '0px';
+    fixedNav.style.position = 'sticky';
+  } else {
+    fixedNav.style.position = 'relative';
+    fixedNav.style.top = '-5px';
+    fixedNav.style.boxShadow = null;
+  }
+  console.log(scrollCount);
 });
